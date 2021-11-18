c3b7ed53427e87c050b36c00938c150878ac4250
ben890524 <ben10723021@gmail.com>
2021-11-19 00:55:03 +0800

    1. jsx

--- kcs/src/App.jsx
+++ kcs/src/App.jsx
@@ -1,13 +1,13 @@
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
 import React from "react";
-
+// import { Helmet } from 'react-helmet'-
 import './App.scss';
-import KCSNavbar from './components/KCSNavbar'
-import KCSFooter from './components/KCSFooter'
-import BloodPressurePage from './components/bloodPressurePage/BloodPressurePage'
-import RecordDietPage from './components/recordDietPage/RecordDietPage'
-import Search from './components/searchPage/SearchPage'
-import PersonalInformationPage from './components/personalInformationPage/PersonalInformationPage'
+import KCSNavbar from './components/KCSNavbar.jsx'
+import KCSFooter from './components/KCSFooter.jsx'
+import BloodPressurePage from './components/bloodPressurePage/BloodPressurePage.jsx'
+import RecordDietPage from './components/recordDietPage/RecordDietPage.jsx'
+import Search from './components/searchPage/SearchPage.jsx'
+import PersonalInformationPage from './components/personalInformationPage/PersonalInformationPage.jsx'
 
 function App() {
 
--- kcs/src/index.js
+++ kcs/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 // Above are buildin dependency when project init.
 
